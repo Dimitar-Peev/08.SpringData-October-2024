d5f3f30b5931c01afe129f5d6ea996ccda8d0a25
Dimitar-Peev <mctrix87@abv.bg>
2024-10-20 08:20:16 +0300

    Add "DbContext.java"

---
+++ _06_Workshop_Custom_ORM/src/main/java/orm/context/DbContext.java
@@ -0,0 +1,23 @@
+package orm.context;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+
+public interface DbContext<E> {
+    boolean persist(E entity) throws IllegalAccessException, SQLException; // INSERT
+
+    void doCreate(Class<E> entityClass) throws SQLException;
+
+    void doAlter(Class<E> entityClass) throws SQLException;
+
+    Iterable<E> find(Class<E> table) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException; // SELECT
+
+    Iterable<E> find(Class<E> table, String where) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException; // SELECT ... WHERE ...
+
+    E findFirst(Class<E> table) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException; // SELECT ... LIMIT 1
+
+    E findFirst(Class<E> table, String where) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException; // SELECT ... WHERE ... LIMIT 1
+
+    int delete(Class<E> table, String where) throws SQLException;
+    int delete(E entity) throws SQLException, IllegalAccessException;
+}
