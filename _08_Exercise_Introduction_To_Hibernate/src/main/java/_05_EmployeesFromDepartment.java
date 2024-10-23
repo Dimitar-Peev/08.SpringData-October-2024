8afc64a56379fd3ee9930721e913384a318735b3
Dimitar-Peev <mctrix87@abv.bg>
2024-10-23 19:03:13 +0300

    Create "_05_EmployeesFromDepartment.java"

---
+++ _08_Exercise_Introduction_To_Hibernate/src/main/java/_05_EmployeesFromDepartment.java
@@ -0,0 +1,12 @@
+import entities.Employee;
+
+public class _05_EmployeesFromDepartment {
+    public static void main(String[] args) {
+        HibernateUtil.createEntityManager()
+                .createQuery("FROM Employee WHERE department.name = :departmentName ORDER BY salary, id",
+                        Employee.class)
+                .setParameter("departmentName", "Research and Development")
+                .getResultList()
+                .forEach(Employee::printFullNameDepartmentNameAndSalary);
+    }
+}
