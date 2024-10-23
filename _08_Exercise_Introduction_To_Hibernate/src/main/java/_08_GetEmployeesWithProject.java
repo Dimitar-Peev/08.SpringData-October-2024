4219e5274f9bb30bf165d2de10f58950e37664a7
Dimitar-Peev <mctrix87@abv.bg>
2024-10-23 19:52:03 +0300

    Create "_08_GetEmployeesWithProject.java"

---
+++ _08_Exercise_Introduction_To_Hibernate/src/main/java/_08_GetEmployeesWithProject.java
@@ -0,0 +1,16 @@
+import entities.Employee;
+
+import java.util.Scanner;
+
+public class _08_GetEmployeesWithProject {
+    public static void main(String[] args) {
+        System.out.println("Enter employee id:");
+        Integer employeeId = new Scanner(System.in).nextInt();
+
+        HibernateUtil.createEntityManager()
+                .createQuery("FROM Employee WHERE id = :employeeId", Employee.class)
+                .setParameter("employeeId", employeeId)
+                .getSingleResult()
+                .printFullNameWithProjectNames();
+    }
+}
