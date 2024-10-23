b41560bf0ac4af5b0619c3d13853f722db344dd3
Dimitar-Peev <mctrix87@abv.bg>
2024-10-23 18:47:50 +0300

    Create "_03_ContainsEmployee.java"

---
+++ _08_Exercise_Introduction_To_Hibernate/src/main/java/_03_ContainsEmployee.java
@@ -0,0 +1,27 @@
+import entities.Employee;
+import entities.Town;
+
+import javax.persistence.EntityManager;
+import java.util.Scanner;
+
+public class _03_ContainsEmployee {
+    public static void main(String[] args) {
+        EntityManager entityManager = HibernateUtil.createEntityManager();
+
+        System.out.println("Enter employee name: ");
+        Scanner scanner = new Scanner(System.in);
+        String fullName = scanner.nextLine();
+
+        entityManager.getTransaction().begin();
+
+        String isEmployeePresented = entityManager
+                .createQuery("FROM Employee WHERE CONCAT_WS(' ', firstName, lastName) = :fullName", Employee.class)
+                .setParameter("fullName", fullName)
+                .getResultList()
+                .isEmpty() ? "No" : "Yes";
+
+        entityManager.getTransaction().commit();
+
+        System.out.println(isEmployeePresented);
+    }
+}
