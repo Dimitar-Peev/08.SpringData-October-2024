6710e5ef4ea9888d8e9ca7abaaf43944e110bd1e
Dimitar-Peev <mctrix87@abv.bg>
2024-10-26 13:48:04 +0300

    Add "Product.java"

---
+++ _10_Exercise_Hibernate_Code_First/src/main/java/entities/sales/Product.java
@@ -0,0 +1,62 @@
+package entities.sales;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Basic
+    private String name;
+
+    @Basic
+    private double quantity;
+
+    @Basic
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
