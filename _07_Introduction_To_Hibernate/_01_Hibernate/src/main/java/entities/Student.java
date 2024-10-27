1e3522211e42038d1b537b71f39c9463e0da35b6
Dimitar-Peev <mctrix87@abv.bg>
2024-10-27 08:03:37 +0200

    Add "Student.java"

---
+++ _07_Introduction_To_Hibernate/_01_Hibernate/src/main/java/entities/Student.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class Student {
+    private long id;
+    private String name;
+
+    public Student() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Student{id=%d, name='%s'}", id, name);
+    }
+}
