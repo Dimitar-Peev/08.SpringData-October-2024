164340bcc821f5ded6127ebb3de935f211663773
Dimitar-Peev <mctrix87@abv.bg>
2024-10-30 23:29:20 +0200

    Add "Application.java"

---
+++ _12_Exercise_Spring_Data_Introduction/src/main/java/org/bookshop/_12_exercise_spring_data_introduction/Application.java
@@ -0,0 +1,13 @@
+package org.bookshop._12_exercise_spring_data_introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
