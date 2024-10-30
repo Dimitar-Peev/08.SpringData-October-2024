ffcbc8dc8ce27bb3b211b0a9c0bc66dc55d60b9d
Dimitar-Peev <mctrix87@abv.bg>
2024-10-30 23:30:38 +0200

    Add "Author.java"

---
+++ _12_Exercise_Spring_Data_Introduction/src/main/java/org/bookshop/_12_exercise_spring_data_introduction/data/entities/Author.java
@@ -0,0 +1,59 @@
+package org.bookshop._12_exercise_spring_data_introduction.data.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Email;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "authors")
+public class Author extends BaseEntity {
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @OneToMany(mappedBy = "author",fetch = FetchType.EAGER)
+    private Set<Book> books;
+
+    @Email(regexp = "", message = "Not valid email")
+    @Pattern(regexp = "", message = "")
+    private String email;
+
+    public Author() {
+        this.books = new HashSet<>();
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return this.books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
