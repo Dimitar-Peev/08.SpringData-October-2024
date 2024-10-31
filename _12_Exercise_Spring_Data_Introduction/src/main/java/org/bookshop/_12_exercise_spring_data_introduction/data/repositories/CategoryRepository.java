5bbbaecf44e155c78ad264c52dc954938920fcb7
Dimitar-Peev <mctrix87@abv.bg>
2024-10-31 17:45:13 +0200

    Add "CategoryRepository.java"

---
+++ _12_Exercise_Spring_Data_Introduction/src/main/java/org/bookshop/_12_exercise_spring_data_introduction/data/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package org.bookshop._12_exercise_spring_data_introduction.data.repositories;
+
+import org.bookshop._12_exercise_spring_data_introduction.data.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
