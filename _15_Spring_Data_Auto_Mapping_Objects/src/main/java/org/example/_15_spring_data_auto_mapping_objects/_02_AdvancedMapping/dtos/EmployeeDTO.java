9bfb8c1cce08241c98b781a0db69d009fea6ff17
Dimitar-Peev <mctrix87@abv.bg>
2024-11-05 23:48:22 +0200

    Add "EmployeeDTO.java"

---
+++ _15_Spring_Data_Auto_Mapping_Objects/src/main/java/org/example/_15_spring_data_auto_mapping_objects/_02_AdvancedMapping/dtos/EmployeeDTO.java
@@ -0,0 +1,38 @@
+package org.example._15_spring_data_auto_mapping_objects._02_AdvancedMapping.dtos;
+
+public class EmployeeDTO {
+    private String firstName;
+    private String lastName;
+    private double salary;
+
+    public EmployeeDTO() {}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %.2f", firstName, lastName, salary);
+    }
+}
