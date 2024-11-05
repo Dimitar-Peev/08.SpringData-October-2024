464bfc1b9b35f1e935e4ed0a467f9abfcc91f91d
Dimitar-Peev <mctrix87@abv.bg>
2024-11-05 23:51:42 +0200

    Add "ProjectionApp.java"

---
+++ _15_Spring_Data_Auto_Mapping_Objects/src/main/java/org/example/_15_spring_data_auto_mapping_objects/_03_Projection/ProjectionApp.java
@@ -0,0 +1,13 @@
+package org.example._15_spring_data_auto_mapping_objects._03_Projection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectionApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectionApp.class, args);
+    }
+
+}
