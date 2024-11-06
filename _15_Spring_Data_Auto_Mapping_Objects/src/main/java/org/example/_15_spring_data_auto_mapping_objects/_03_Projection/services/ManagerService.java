368db2b6d1c3f154c04f6cb40f2c8ea8d7a4132c
Dimitar-Peev <mctrix87@abv.bg>
2024-11-06 17:25:23 +0200

    Add "ManagerService.java"

---
+++ _15_Spring_Data_Auto_Mapping_Objects/src/main/java/org/example/_15_spring_data_auto_mapping_objects/_03_Projection/services/ManagerService.java
@@ -0,0 +1,9 @@
+package org.example._15_spring_data_auto_mapping_objects._03_Projection.services;
+
+import org.example._15_spring_data_auto_mapping_objects._03_Projection.entities.dtos.EmployeeDTO;
+
+import java.util.List;
+
+public interface ManagerService {
+    List<EmployeeDTO> findAllManagers();
+}
