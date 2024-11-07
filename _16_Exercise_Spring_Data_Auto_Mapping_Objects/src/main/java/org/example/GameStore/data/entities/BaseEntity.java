703845db5b27bbcd46144abad0333de1c7ae61fa
Dimitar-Peev <mctrix87@abv.bg>
2024-11-07 17:00:35 +0200

    Add "BaseEntity.java"

---
+++ _16_Exercise_Spring_Data_Auto_Mapping_Objects/src/main/java/org/example/GameStore/data/entities/BaseEntity.java
@@ -0,0 +1,21 @@
+package org.example.GameStore.data.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    protected BaseEntity() {
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+}
