4ca10fb22bb95598a6ec8bd58f79ce28bf6bb31e
Dimitar-Peev <mctrix87@abv.bg>
2024-11-07 17:04:01 +0200

    Add "ValidationUtil.java"

---
+++ _16_Exercise_Spring_Data_Auto_Mapping_Objects/src/main/java/org/example/GameStore/utils/ValidationUtil.java
@@ -0,0 +1,12 @@
+package org.example.GameStore.utils;
+
+import jakarta.validation.ConstraintViolation;
+
+import java.util.Set;
+
+public interface ValidationUtil {
+
+    <E> boolean isValid(E entity);
+
+    <E> Set<ConstraintViolation<E>> validate(E entity);
+}
