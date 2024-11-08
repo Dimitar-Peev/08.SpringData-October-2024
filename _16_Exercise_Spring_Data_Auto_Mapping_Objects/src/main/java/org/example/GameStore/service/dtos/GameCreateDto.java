7fd882f8fae31ff81726207fda295c84747e0e30
Dimitar-Peev <mctrix87@abv.bg>
2024-11-08 19:35:37 +0200

    Add "GameCreateDto.java"

---
+++ _16_Exercise_Spring_Data_Auto_Mapping_Objects/src/main/java/org/example/GameStore/service/dtos/GameCreateDto.java
@@ -0,0 +1,92 @@
+package org.example.GameStore.service.dtos;
+
+import jakarta.validation.constraints.Positive;
+import org.hibernate.validator.constraints.Length;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class GameCreateDto {
+    @Length(min = 3, max = 100, message = "Title must have a length between 3 and 100 symbols (inclusively).")
+    private String title;
+    @Positive(message = "Price must be a positive number.")
+    private BigDecimal price;
+    @Positive(message = "Size must be a positive number.")
+    private Double size;
+    @Length(min = 11, max = 11,message = "Trailer must be exactly 11 characters.")
+    private String trailer;
+
+    private String imageThumbnail;
+    @Length(min = 20)
+    private String description;
+    private LocalDate releaseDate;
+
+    public GameCreateDto() {
+    }
+
+    public GameCreateDto(String title, BigDecimal price, Double size, String trailer, String imageThumbnail, String description, LocalDate releaseDate) {
+        this.price = price;
+        this.title = title;
+        this.size = size;
+        this.trailer = trailer;
+        this.imageThumbnail = imageThumbnail;
+        this.description = description;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public Double getSize() {
+        return this.size;
+    }
+
+    public String getTrailer() {
+        return this.trailer;
+    }
+
+    public String getImageThumbnail() {
+        return this.imageThumbnail;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public LocalDate getReleaseDate() {
+        return this.releaseDate;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setSize(Double size) {
+        this.size = size;
+    }
+
+    public void setTrailer(String trailer) {
+        this.trailer = trailer;
+    }
+
+    public void setImageThumbnail(String imageThumbnail) {
+        this.imageThumbnail = imageThumbnail;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
