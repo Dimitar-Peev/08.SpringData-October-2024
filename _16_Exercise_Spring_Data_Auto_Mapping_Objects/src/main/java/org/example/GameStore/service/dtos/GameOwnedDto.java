2b7d72fa5be2b740e23876f29b4636bbca40de11
Dimitar-Peev <mctrix87@abv.bg>
2024-11-08 19:36:29 +0200

    Add "GameOwnedDto.java"

---
+++ _16_Exercise_Spring_Data_Auto_Mapping_Objects/src/main/java/org/example/GameStore/service/dtos/GameOwnedDto.java
@@ -0,0 +1,16 @@
+package org.example.GameStore.service.dtos;
+
+public class GameOwnedDto {
+    private String title;
+
+    public GameOwnedDto() {
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
