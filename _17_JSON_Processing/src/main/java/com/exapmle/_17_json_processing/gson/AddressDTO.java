3c752b02255a68bfbd451b9eace1732190c737e6
Dimitar-Peev <mctrix87@abv.bg>
2024-11-09 13:20:54 +0200

    Add "AddressDTO.java"

---
+++ _17_JSON_Processing/src/main/java/com/exapmle/_17_json_processing/gson/AddressDTO.java
@@ -0,0 +1,25 @@
+package com.exapmle._17_json_processing.gson;
+
+import com.google.gson.annotations.Expose;
+
+import java.io.Serializable;
+
+public class AddressDTO implements Serializable {
+    @Expose
+    private String country;
+    @Expose
+    private String city;
+
+    public AddressDTO(String country, String city) {
+        this.country = country;
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressDTO{" +
+                "country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
