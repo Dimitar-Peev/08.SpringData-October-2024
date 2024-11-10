01972c5279c5e36d0aec4d725281505ddb10f411
Dimitar-Peev <mctrix87@abv.bg>
2024-11-10 16:55:30 +0200

    Add "Product.java"

---
+++ _18_Exercise_JSON_Processing/src/main/java/com/example/productsshop/data/entities/Product.java
@@ -0,0 +1,79 @@
+package com.example.productsshop.data.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity {
+
+    @Column
+    private String name;
+
+    @Column
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "buyer_id", referencedColumnName = "id")
+    private User buyer;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id", referencedColumnName = "id", nullable = false)
+    private User seller;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "products_categories",
+            joinColumns = @JoinColumn(name = "product_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id", referencedColumnName = "id"))
+    private Set<Category> categories;
+
+
+    public Product() {
+        this.categories = new HashSet<>();
+    }
+
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public User getBuyer() {
+        return this.buyer;
+    }
+
+    public void setBuyer(User buyer) {
+        this.buyer = buyer;
+    }
+
+    public User getSeller() {
+        return this.seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public Set<Category> getCategories() {
+        return this.categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+}
