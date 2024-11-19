119747a7f1c19cef4635670d86b46ff82ebdcd02
Dimitar-Peev <mctrix87@abv.bg>
2024-11-19 21:30:21 +0200

    Add "Address.java"

---
+++ _19_XML_Processing/src/main/java/com/example/_19_xml_processing/jaxb/entities/Address.java
@@ -0,0 +1,18 @@
+package com.example._19_xml_processing.jaxb.entities;
+
+import jakarta.xml.bind.annotation.XmlElement;
+
+public class Address {
+    @XmlElement
+    private String country;
+
+    @XmlElement
+    private String city;
+
+    public Address() {}
+
+    public Address(String country, String city) {
+        this.country = country;
+        this.city = city;
+    }
+}
