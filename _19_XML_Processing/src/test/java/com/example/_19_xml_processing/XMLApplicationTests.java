40d7f4fc28217ac46eee1d5ecbc40491e88a8bca
Dimitar-Peev <mctrix87@abv.bg>
2024-11-20 19:18:57 +0200

    Add "XMLApplicationTests.java"

---
+++ _19_XML_Processing/src/test/java/com/example/_19_xml_processing/XMLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._19_xml_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XMLApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
