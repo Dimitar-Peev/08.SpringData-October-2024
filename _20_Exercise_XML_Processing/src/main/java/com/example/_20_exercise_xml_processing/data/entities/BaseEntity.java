ef4b308f028a816b0f184b875841778d4351370a
Dimitar-Peev <mctrix87@abv.bg>
2024-11-22 20:16:32 +0200

    Add "BaseEntity.java"

---
+++ _20_Exercise_XML_Processing/src/main/java/com/example/_20_exercise_xml_processing/data/entities/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.example._20_exercise_xml_processing.data.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    protected BaseEntity() {}
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
