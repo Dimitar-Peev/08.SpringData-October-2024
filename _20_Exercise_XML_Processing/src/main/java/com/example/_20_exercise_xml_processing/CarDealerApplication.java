6c64a4b7e14be3b5b230c8ef486025a34b21db7d
Dimitar-Peev <mctrix87@abv.bg>
2024-11-22 20:16:07 +0200

    Add "CarDealerApplication.java"

---
+++ _20_Exercise_XML_Processing/src/main/java/com/example/_20_exercise_xml_processing/CarDealerApplication.java
@@ -0,0 +1,13 @@
+package com.example._20_exercise_xml_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarDealerApplication.class, args);
+    }
+
+}
