17de3a3547f511788f6f4a1af42f77a615a3d0d7
Dimitar-Peev <mctrix87@abv.bg>
2024-11-25 22:28:53 +0200

    Add "CarSeedDto.java"

---
+++ _20_Exercise_XML_Processing/src/main/java/com/example/_20_exercise_xml_processing/data/dtos/imports/CarSeedDto.java
@@ -0,0 +1,47 @@
+package com.example._20_exercise_xml_processing.data.dtos.imports;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "car")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CarSeedDto  {
+
+    @XmlElement
+    private String make;
+
+    @XmlElement
+    private String model;
+
+    @XmlElement(name = "travelled-distance")
+    private long travelledDistance;
+
+    public CarSeedDto() {
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public long getTravelledDistance() {
+        return this.travelledDistance;
+    }
+
+    public void setTravelledDistance(long travelledDistance) {
+        this.travelledDistance = travelledDistance;
+    }
+}
