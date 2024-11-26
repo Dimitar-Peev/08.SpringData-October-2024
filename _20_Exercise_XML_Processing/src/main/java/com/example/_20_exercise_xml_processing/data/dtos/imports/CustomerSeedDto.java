d838fcc7a710cbca55a72e4ea17527094ca82349
Dimitar-Peev <mctrix87@abv.bg>
2024-11-26 21:52:03 +0200

    Add "CustomerSeedDto.java"

---
+++ _20_Exercise_XML_Processing/src/main/java/com/example/_20_exercise_xml_processing/data/dtos/imports/CustomerSeedDto.java
@@ -0,0 +1,44 @@
+package com.example._20_exercise_xml_processing.data.dtos.imports;
+
+import jakarta.xml.bind.annotation.*;
+
+@XmlRootElement(name = "customer")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CustomerSeedDto {
+
+    @XmlAttribute
+    private String name;
+
+    @XmlElement(name = "birth-date")
+    private String birthDate;
+
+    @XmlElement(name = "is-young-driver")
+    private boolean isYoungDriver;
+
+    public CustomerSeedDto() {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isYoungDriver() {
+        return this.isYoungDriver;
+    }
+
+    public void setYoungDriver(boolean youngDriver) {
+        isYoungDriver = youngDriver;
+    }
+}
