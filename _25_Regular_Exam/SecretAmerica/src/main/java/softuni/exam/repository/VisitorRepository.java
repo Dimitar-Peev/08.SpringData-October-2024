bfcaad13adbcbce772de0011ab81582788c71f6a
Dimitar-Peev <mctrix87@abv.bg>
2024-12-01 09:53:37 +0200

    Add "VisitorRepository.java"

---
+++ _25_Regular_Exam/SecretAmerica/src/main/java/softuni/exam/repository/VisitorRepository.java
@@ -0,0 +1,13 @@
+package softuni.exam.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.exam.models.entity.Visitor;
+
+@Repository
+public interface VisitorRepository extends JpaRepository<Visitor, Long> {
+
+    boolean existsByFirstNameAndLastName(String firstName, String lastName);
+
+    boolean existsVisitorByPersonalDataId(Long personalDataId);
+}
