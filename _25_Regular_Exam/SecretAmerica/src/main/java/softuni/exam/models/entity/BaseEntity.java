fe2784f2622ccb8852e6239514c9f2df32dcd732
Dimitar-Peev <mctrix87@abv.bg>
2024-12-01 09:11:54 +0200

    Add "BaseEntity.java"

---
+++ _25_Regular_Exam/SecretAmerica/src/main/java/softuni/exam/models/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package softuni.exam.models.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
