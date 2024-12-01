fb2fd881dc85c6519113321cb1928e7b61d95dc3
Dimitar-Peev <mctrix87@abv.bg>
2024-12-01 09:45:58 +0200

    Add "PersonalDataSeedRootDto.java"

---
+++ _25_Regular_Exam/SecretAmerica/src/main/java/softuni/exam/models/dto/xml/PersonalDataSeedRootDto.java
@@ -0,0 +1,22 @@
+package softuni.exam.models.dto.xml;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@XmlRootElement(name = "personal_datas")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PersonalDataSeedRootDto {
+
+    @XmlElement(name = "personal_data")
+    private List<PersonalDataSeedDto> personalDataSeedDtoList;
+}
