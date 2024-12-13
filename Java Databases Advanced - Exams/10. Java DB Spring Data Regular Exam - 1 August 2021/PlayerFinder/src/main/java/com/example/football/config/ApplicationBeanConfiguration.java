c63eabf3230d1a6f0a67020bbc133f56e7ece312
Dimitar-Peev <mctrix87@abv.bg>
2024-12-13 19:29:42 +0200

    Add "ApplicationBeanConfiguration.java"

---
+++ Java Databases Advanced - Exams/10. Java DB Spring Data Regular Exam - 1 August 2021/PlayerFinder/src/main/java/com/example/football/config/ApplicationBeanConfiguration.java
@@ -0,0 +1,45 @@
+package com.example.football.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class ApplicationBeanConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.addConverter(new Converter<String, LocalDate>() {
+            @Override
+            public LocalDate convert(MappingContext<String, LocalDate> mappingContext) {
+                return LocalDate.parse(mappingContext.getSource(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            }
+        });
+
+        return modelMapper;
+    }
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                .setPrettyPrinting()
+                .excludeFieldsWithoutExposeAnnotation()
+                .create();
+    }
+
+    @Bean
+    public Validator validator() {
+        return Validation.buildDefaultValidatorFactory().getValidator();
+    }
+}
