232f05450dd50fab0a1f215b3a3eeaf3a67c8926
Dimitar-Peev <mctrix87@abv.bg>
2024-12-14 22:19:07 +0200

    Add "GlobalConstants.java"

---
+++ Java Databases Advanced - Exams/15. Java DB Advanced Exam - 20 Dec 2018/HiberspringInc/src/main/java/hiberspring/common/GlobalConstants.java
@@ -0,0 +1,13 @@
+package hiberspring.common;
+
+public final class GlobalConstants {
+
+    public static final String TOWNS_FILE_PATH = "src/main/resources/files/towns.json";
+    public static final String BRANCHES_FILE_PATH = "src/main/resources/files/branches.json";
+    public static final String EMPLOYEE_CARDS_FILE_PATH = "src/main/resources/files/employee-cards.json";
+
+    public static final String PRODUCTS_FILE_PATH = "src/main/resources/files/products.xml";
+    public static final String EMPLOYEES_FILE_PATH = "src/main/resources/files/employees.xml";
+
+    public final static String INCORRECT_DATA_MESSAGE = "Error: Invalid data.";
+}
