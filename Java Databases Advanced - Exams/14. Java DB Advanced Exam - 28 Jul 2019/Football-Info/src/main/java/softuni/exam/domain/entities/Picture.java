19363d115f814fb00e813afabe350a4fb89c7cc4
Dimitar-Peev <mctrix87@abv.bg>
2024-12-18 19:02:58 +0200

    Update "Picture.java"

--- Java Databases Advanced - Exams/14. Java DB Advanced Exam - 28 Jul 2019/Football-Info/src/main/java/softuni/exam/domain/entities/Picture.java
+++ Java Databases Advanced - Exams/14. Java DB Advanced Exam - 28 Jul 2019/Football-Info/src/main/java/softuni/exam/domain/entities/Picture.java
@@ -2,10 +2,13 @@ package softuni.exam.domain.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -17,7 +20,6 @@ public class Picture extends BaseEntity {
     @Column(name = "url", nullable = false)
     private String url;
 
-    // One To Many - bidirectional, must be collection ! Set OR List
-//    @OneToMany(mappedBy = "picture", targetEntity = Team.class)
-//    private Set<Team> teamSet;
+    @OneToMany(mappedBy = "picture", targetEntity = Team.class)
+    private Set<Team> teamSet;
 }
