07b924f91f1cd0307fadf5329df6e1e0b42cff68
Dimitar-Peev <mctrix87@abv.bg>
2024-12-20 17:07:52 +0200

    Add folder "util"

---
+++ Java Databases Advanced - Exams/12. Java DB Spring Data Retake Exam - 3 April 2020/Airline/src/main/java/softuni/exam/util/XmlParserImpl.java
@@ -0,0 +1,21 @@
+package softuni.exam.util;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+import org.springframework.stereotype.Component;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+@Component
+public class XmlParserImpl implements XmlParser {
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T convertFromFile(String filePath, Class<T> clazz) throws JAXBException, FileNotFoundException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        return (T) unmarshaller.unmarshal(new FileReader(filePath));
+    }
+}
