162e7434b0954d989dd1dd8995c1918db3dd039b
Dimitar-Peev <mctrix87@abv.bg>
2024-12-21 15:12:51 +0200

    Update "PlaneSeedRootDto.java"

--- Java Databases Advanced - Exams/12. Java DB Spring Data Retake Exam - 3 April 2020/Airline/src/main/java/softuni/exam/models/dtos/xml/PlaneSeedRootDto.java
+++ Java Databases Advanced - Exams/12. Java DB Spring Data Retake Exam - 3 April 2020/Airline/src/main/java/softuni/exam/models/dtos/xml/PlaneSeedRootDto.java
@@ -4,20 +4,19 @@ import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @XmlRootElement(name = "planes")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class PlaneSeedRootDto {
 
     @XmlElement(name = "plane")
     private List<PlaneSeedDto> planes;
-
-    public List<PlaneSeedDto> getPlanes() {
-        return this.planes;
-    }
-
-    public void setPlanes(List<PlaneSeedDto> planes) {
-        this.planes = planes;
-    }
 }
