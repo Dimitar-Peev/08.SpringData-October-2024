37f5298c2ac9a4ed36e04ec65a0061563eb89292
Dimitar-Peev <mctrix87@abv.bg>
2024-12-25 18:08:28 +0200

    Add "LibraryMember.java"

---
+++ Java Databases Advanced - Exams/03. Java DB Spring Data Retake Exam - 13 August 2023/Library/src/main/java/softuni/exam/models/entity/LibraryMember.java
@@ -0,0 +1,34 @@
+package softuni.exam.models.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "library_members")
+public class LibraryMember extends BaseEntity {
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone_number", unique = true, nullable = false)
+    private String phoneNumber;
+
+    @OneToMany(mappedBy = "member")
+    private Set<BorrowingRecord> borrowingRecords;
+}
