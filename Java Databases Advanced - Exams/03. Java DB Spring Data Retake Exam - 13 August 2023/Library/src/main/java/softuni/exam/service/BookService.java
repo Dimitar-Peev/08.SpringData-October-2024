e1f4eb58b1d4c0eb7e212bd52f039c41565f0b7f
Dimitar-Peev <mctrix87@abv.bg>
2024-12-26 16:29:01 +0200

    Add "BookService.java"

---
+++ Java Databases Advanced - Exams/03. Java DB Spring Data Retake Exam - 13 August 2023/Library/src/main/java/softuni/exam/service/BookService.java
@@ -0,0 +1,12 @@
+package softuni.exam.service;
+
+import java.io.IOException;
+
+public interface BookService {
+
+    boolean areImported();
+
+    String readBooksFromFile() throws IOException;
+
+	String importBooks() throws IOException;
+}
