bf191c175988acdc1cadf7c21182ee593f6a634d
Dimitar-Peev <mctrix87@abv.bg>
2024-12-28 18:10:30 +0200

    Add "BookImportDto.java"

---
+++ Java Databases Advanced - Exams/03. Java DB Spring Data Retake Exam - 13 August 2023/Library/src/main/java/softuni/exam/models/dto/xml/BookImportDto.java
@@ -0,0 +1,19 @@
+package softuni.exam.models.dto.xml;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Getter
+@Setter
+@XmlRootElement(name = "book")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class BookImportDto {
+
+    @XmlElement(name = "title")
+    private String title;
+}
