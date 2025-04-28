76de4025807e0777e408eac690de70330e4761e9
Dimitar-Peev <mctrix87@abv.bg>
2025-04-28 07:16:43 +0300

    Add "XmlParserImpl.java"

---
+++ Java Databases Advanced - Exams/13. Java DB Spring Data Regular Exam - 29 March 2020/RealDeal/src/main/java/softuni/exam/util/impl/XmlParserImpl.java
@@ -0,0 +1,24 @@
+package softuni.exam.util.impl;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+import org.springframework.stereotype.Component;
+import softuni.exam.util.XmlParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+@Component
+public class XmlParserImpl implements XmlParser {
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T convertFromFile(String filePath, Class<T> clazz) throws JAXBException, FileNotFoundException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        FileReader fileReader = new FileReader(filePath);
+        return (T) unmarshaller.unmarshal(fileReader);
+    }
+
+}
