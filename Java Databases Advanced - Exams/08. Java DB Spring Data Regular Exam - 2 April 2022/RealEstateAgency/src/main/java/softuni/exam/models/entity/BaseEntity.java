39768573949638f3c5ecde2ded3866114277d011
Dimitar-Peev <mctrix87@abv.bg>
2025-05-01 09:03:30 +0300

    Add "BaseEntity.java"

---
+++ Java Databases Advanced - Exams/08. Java DB Spring Data Regular Exam - 2 April 2022/RealEstateAgency/src/main/java/softuni/exam/models/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package softuni.exam.models.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
