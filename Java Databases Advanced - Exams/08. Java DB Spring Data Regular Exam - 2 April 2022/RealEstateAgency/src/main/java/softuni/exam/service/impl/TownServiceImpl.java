9e6a33f671ce792880ab05481f029b03aeb4e52c
Dimitar-Peev <mctrix87@abv.bg>
2025-05-02 08:12:54 +0300

    Add "TownServiceImpl.java"

---
+++ Java Databases Advanced - Exams/08. Java DB Spring Data Regular Exam - 2 April 2022/RealEstateAgency/src/main/java/softuni/exam/service/impl/TownServiceImpl.java
@@ -0,0 +1,69 @@
+package softuni.exam.service.impl;
+
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import softuni.exam.models.dto.json.TownSeedDto;
+import softuni.exam.models.entity.Town;
+import softuni.exam.repository.TownRepository;
+import softuni.exam.service.TownService;
+import softuni.exam.util.ValidationUtil;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+@Service
+public class TownServiceImpl implements TownService {
+
+    private static final String FILE_PATH = "src/main/resources/files/json/towns.json";
+
+    private final TownRepository townRepository;
+    private final ModelMapper modelMapper;
+    private final ValidationUtil validationUtil;
+    private final Gson gson;
+
+    @Autowired
+    public TownServiceImpl(TownRepository townRepository, ModelMapper modelMapper, ValidationUtil validationUtil, Gson gson) {
+        this.townRepository = townRepository;
+        this.modelMapper = modelMapper;
+        this.validationUtil = validationUtil;
+        this.gson = gson;
+    }
+
+    @Override
+    public boolean areImported() {
+        return this.townRepository.count() > 0;
+    }
+
+    @Override
+    public String readTownsFileContent() throws IOException {
+        return Files.readString(Path.of(FILE_PATH));
+    }
+
+    @Override
+    public String importTowns() throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        TownSeedDto[] townSeedDtos = this.gson.fromJson(readTownsFileContent(), TownSeedDto[].class);
+
+        Arrays.stream(townSeedDtos)
+                .filter(townSeedDto -> {
+                    boolean isValid = this.validationUtil.isValid(townSeedDto);
+
+                    sb.append(isValid
+                                    ? String.format("Successfully imported town %s - %d",
+                                    townSeedDto.getTownName(), townSeedDto.getPopulation())
+                                    : "Invalid town")
+                            .append(System.lineSeparator());
+
+                    return isValid;
+                })
+                .map(townSeedDto -> modelMapper.map(townSeedDto, Town.class))
+                .forEach(townRepository::save);
+
+        return sb.toString().trim();
+    }
+}
