ec38636c6d4570c9a7dd6748e2d4d42ea5d898ce
Dimitar-Peev <mctrix87@abv.bg>
2025-05-05 20:50:49 +0300

    Add "OfferService.java"

---
+++ Java Databases Advanced - Exams/08. Java DB Spring Data Regular Exam - 2 April 2022/RealEstateAgency/src/main/java/softuni/exam/service/OfferService.java
@@ -0,0 +1,15 @@
+package softuni.exam.service;
+
+import jakarta.xml.bind.JAXBException;
+import java.io.IOException;
+
+public interface OfferService {
+
+    boolean areImported();
+
+    String readOffersFileContent() throws IOException;
+	
+	String importOffers() throws IOException, JAXBException;
+
+    String exportOffers();
+}
