e5137174eb3da4e81ad88f817b1a708096b75484
Dimitar-Peev <mctrix87@abv.bg>
2025-05-10 16:27:38 +0300

    Add "Messages.java"

---
+++ Java Databases Advanced - Exams/07. Java DB Spring Data Retake Exam - 17 April 2022/SoftWeatherForecast/src/main/java/softuni/exam/constants/Messages.java
@@ -0,0 +1,13 @@
+package softuni.exam.constants;
+
+public enum Messages {
+    ;
+    public static final String INVALID_COUNTRY = "Invalid country";
+    public static final String VALID_COUNTRY = "Successfully imported country %s - %s";
+
+    public static final String INVALID_CITY = "Invalid city";
+    public static final String VALID_CITY = "Successfully imported city %s - %s";
+
+    public static final String INVALID_FORECAST = "Invalid forecast";
+    public static final String VALID_FORECAST = "Successfully import forecast %s - %s";
+}
