e7d3eadd6f6005e20de88a6672b851ec680215b8
Dimitar-Peev <mctrix87@abv.bg>
2025-05-10 16:27:54 +0300

    Add "Paths.java"

---
+++ Java Databases Advanced - Exams/07. Java DB Spring Data Retake Exam - 17 April 2022/SoftWeatherForecast/src/main/java/softuni/exam/constants/Paths.java
@@ -0,0 +1,10 @@
+package softuni.exam.constants;
+
+import java.nio.file.Path;
+
+public enum Paths {
+    ;
+    public static final Path COUNTRIES_PATH = Path.of("src/main/resources/files/json/countries.json");
+    public static final Path CITIES_PATH = Path.of("src/main/resources/files/json/cities.json");
+    public static final String FORECASTS_PATH = "src/main/resources/files/xml/forecasts.xml";
+}
